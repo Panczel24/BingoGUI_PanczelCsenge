c44e334618d48c886c958132b36dab84c201d12c
Panczel24 <pcsenge8@gmail.com>
2026-03-04 10:22:12 +0100

    Add project files.

---
+++ BingoGUI_PanczelCsenge/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BingoGUI_PanczelCsenge
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
